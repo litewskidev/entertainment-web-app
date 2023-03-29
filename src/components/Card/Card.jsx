f9cf4ac9aeedffe18e225c6627bf040412b4de60
Adrian Litewski <litewskidev@gmail.com>
2023-03-30 00:14:08 +0200

    add toggle functionality & toggle icons

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,20 +1,34 @@
-import './Card.scss';
+import styles from './Card.module.scss';
+import { useDispatch } from 'react-redux';
+import { toggleBookmark } from '../../redux/listRedux';
+import ToggleBookmarkIcon from '../ToggleBookmarkIcon/ToggleBookmarkIcon';
+import clsx from 'clsx';
 
-const Card = ({title, id, year, category, rating}) => {
+const Card = ({title, id, year, category, rating, isBookmarked}) => {
+
+  const dispatch = useDispatch();
+
+  const toggle = () => {
+    dispatch(toggleBookmark(id, isBookmarked));
+  };
+
   return (
-    <div className='list__card'>
-      <div className='card__image'>
+    <div className={styles.list__card}>
+      <div className={styles.card__image}>
         <img alt={title} src={`${process.env.PUBLIC_URL}/assets/${id}/regular/large.jpg`} />
       </div>
-      <div className='card__description body__S'>
+      <div className={clsx(styles.card__description, 'body__S')}>
         <p>{year}</p>
         <p>•</p>
         <p>{category}</p>
         <p>•</p>
         <p>{rating}</p>
       </div>
-      <div className='card__title heading__XS'>
+      <div className={clsx(styles.card__title, 'heading__XS')}>
         <p>{title}</p>
+      </div>
+      <div className={styles.inner__card}>
+        <button className={styles.bookBtn} onClick={toggle}><ToggleBookmarkIcon active={isBookmarked} /></button>
       </div>
     </div>
   );
---
+++ src/components/ToggleBookmarkIcon/ToggleBookmarkIcon.jsx
@@ -0,0 +1,19 @@
+import './ToggleBookmarkIcon.scss';
+
+const ToggleBookmarkIcon = ({ active }) => {
+
+  const activeMark = `${process.env.PUBLIC_URL}/assets/icons/icon-bookmark-full.svg`;
+  const inactiveMark = `${process.env.PUBLIC_URL}/assets/icons/icon-bookmark-empty.svg`;
+
+  return(
+    <div className='toggle'>
+      {active ? (
+        <img alt='active icon' src={activeMark} className='active' />
+      ) : (
+        <img alt='inactive icon' src={inactiveMark} className='inactive' />
+      )};
+    </div>
+  );
+};
+
+export default ToggleBookmarkIcon;
